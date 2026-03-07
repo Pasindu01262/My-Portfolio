baea0a3016f2e3e34957e3846d3868c287facb95
pasindu Ransika <pasindukossinna@gmail.com>
2026-03-08 00:01:20 +0845

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,6 +4,9 @@ import project3_img from '../assets/project_3.png'
 import project4_img from '../assets/project_4.png'
 import project5_img from '../assets/project_5.png'
 import project6_img from '../assets/project_6.png'
+import project6_img from '../assets/project_7.png'
+import project6_img from '../assets/project_8.png'
+import project6_img from '../assets/project_9.png'
 
 const mywork_data = [
     {
@@ -35,6 +38,21 @@ const mywork_data = [
         w_no:6,
         w_name:"Web design",
         w_img:project6_img
+    },
+    {
+        w_no:7,
+        w_name:"Web design",
+        w_img:project7_img
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project8_img
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project9_img
     },
 ]
  
