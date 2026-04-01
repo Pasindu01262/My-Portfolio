87e1a893ffeabe33869d7f54625ef9e59c8a948d
Pasindu Ransika Kossinna <196893365+Pasindu01262@users.noreply.github.com>
2026-04-01 16:02:30 +0845

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -12,47 +12,56 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        w_link:"https://github.com/Pasindu01262/project-02-hotel"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        w_link:"https://github.com/Pasindu01262/portfolio"
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_img,
+        w_link:"https://university-p.vercel.app/"
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_img,
+        w_link:"https://www.figma.com/design/mGzHakugT3OplDHw3uzAbp/Untitled?node-id=0-1&p=f&t=2MWGsIGN2RcwcJde-0"
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project5_img,
+        w_link:"https://github.com/Pasindu01262/My-Portfolio/blob/main/src/assets/project_5.png"
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_img,
+        w_link:"https://github.com/Pasindu01262/My-Portfolio/blob/main/src/assets/project_6.png"
     },
     {
         w_no:7,
         w_name:"Web design",
-        w_img:project7_img
+        w_img:project7_img,
+        w_link:"https://github.com/Pasindu01262/ecommerce-admin"
     },
     {
-        w_no:6,
+        w_no:8,
         w_name:"Web design",
-        w_img:project8_img
+        w_img:project8_img,
+        w_link:"https://github.com/Pasindu01262/ecommerce-frontend"
     },
     {
-        w_no:6,
+        w_no:9,
         w_name:"Web design",
-        w_img:project9_img
+        w_img:project9_img,
+        w_link:"https://github.com/Pasindu01262/ecommerce-frontend"
     },
 ]
  
